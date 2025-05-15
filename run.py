de706f07f89644888c3d28a9aca63ed51f076a92
timor <timormat321@gmail.com>
2025-05-15 09:52:07 +0300

    init run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+print("START PROGRAM")
+
+loop=True;
+DEBUG=True;
+#connect to WIFI
+
+while loop:
+    
+    # here the taking pictures stuff - DAR
+    
+    
+    
+    
+    #here calling to api
+    
+    
+
+#if here some BUG occured
